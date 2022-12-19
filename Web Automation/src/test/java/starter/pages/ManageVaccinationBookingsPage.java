3080b5f334072e501d88bff429de34f959c83dcd
sarahmargaretha <sarahmargareth2003@gmail.com>
2022-12-19 13:07:41 +0700

    Web Automation #3

--- Web Automation/src/test/java/starter/pages/ManageVaccinationBookingsPage.java
+++ Web Automation/src/test/java/starter/pages/ManageVaccinationBookingsPage.java
@@ -4,11 +4,14 @@ import net.thucydides.core.pages.PageObject;
 import org.openqa.selenium.By;
 
 public class ManageVaccinationBookingsPage extends PageObject {
+    private By loginPage() {
+        return By.xpath("//button[@class='MuiButtonBase-root MuiButton-root MuiButton-outlined MuiButton-outlinedPrimary MuiButton-sizeMedium MuiButton-outlinedSizeMedium MuiButton-root MuiButton-outlined MuiButton-outlinedPrimary MuiButton-sizeMedium MuiButton-outlinedSizeMedium css-51t0sx']");
+    }
     private By emailField() {
-        return By.id(":r0:");
+        return By.xpath("//input[@name='email']");
     }
     private By passwordField() {
-        return By.id(":r1:");
+        return By.xpath("//input[@name='password']");
     }
     private By loginButton() {
         return By.xpath("//button[@class='MuiButtonBase-root MuiButton-root MuiButton-contained MuiButton-containedPrimary MuiButton-sizeMedium MuiButton-containedSizeMedium MuiButton-root MuiButton-contained MuiButton-containedPrimary MuiButton-sizeMedium MuiButton-containedSizeMedium css-j3w5ja']");
@@ -16,7 +19,7 @@ public class ManageVaccinationBookingsPage extends PageObject {
     private By bookVaksinasiPage() {
         return By.xpath("//a[@href='/manage-booking']");
     }
-    private By session() {
+    private By unconfirmedSession() {
         return By.xpath("//div[@class='MuiCardContent-root css-1qw96cp']");
     }
     private By checkButton() {
@@ -27,10 +30,149 @@ public class ManageVaccinationBookingsPage extends PageObject {
         return By.xpath("//button[@fdprocessedid='xn9icp']");
     }
     private By approved() {
-        return By.xpath("//span[@class='MuiChip-label MuiChip-labelMedium css-14vsv3w']");
+        return By.xpath("//div[@class='MuiChip-root MuiChip-filled MuiChip-sizeMedium MuiChip-colorSoftSuccess MuiChip-filledSoftSuccess css-15bit62']");
+        //span[@class='MuiChip-label MuiChip-labelMedium css-14vsv3w']
     }
     private By rejected() {
-        return
+        return By.xpath("//div[@class='MuiChip-root MuiChip-filled MuiChip-sizeMedium MuiChip-colorSoftDanger MuiChip-filledSoftDanger css-t15oan']");
+    }
+    private By checkboxAllBookings() {
+        return By.xpath("//input[@aria-label='Select all rows']");
+    }
+    private By terimaSemuaButton() {
+        return By.xpath("//button[@fdprocessedid='my7ng']");
+    }
+    private By tolakSemuaButton() {
+        return By.xpath("//button[@fdprocessedid='hgjdta']");
+    }
+    private By calendar() {
+        return By.xpath("//button[@class='MuiButtonBase-root MuiIconButton-root MuiIconButton-edgeEnd MuiIconButton-sizeMedium css-slyssw']");
+    }
+    private By availableDate() {
+        return By.xpath("//button[@fdprocessedid='8glucq']");
+    }
+    private By unavailableDate() {
+        return By.xpath("//button[@fdprocessedid='9smnk8']");
+    }
+    private By availableDateSession() {
+        return By.xpath("//h6");
+    }
+    private By unavailableDateSession() {
+        return By.xpath("//img[@src='/static/media/notfound.aa0ca37c75187772b73a.png']");
+    }
+    private By vaccineFilter() {
+        return By.xpath("//div[@fdprocessedid='p0urda']");
+    }
+    private By vaccine() {
+        return By.xpath("//li[@data-value='AstraZeneca']");
+    }
+    private By selectedVaccine() {
+        return By.xpath("//h6");
+    }
+    private By statusFilter() {
+        return By.xpath("//div[@fdprocessedid='xb3zh9']");
+    }
+    private By status() {
+        return By.xpath("//li[@data-value='Berlangsung']");
+    }
+    private By selectedStatus() {
+        return By.xpath("//span[@class='MuiChip-label MuiChip-labelMedium css-14vsv3w']");
+    }
+    private By filterButton() {
+        return By.id("more-filter-button");
+    }
+    //private By timeFilter() {
+        //return By.id("button-filter-waktu");
+    //}
+    //private By time() {
+      //  return By.xpath("//li[@class='MuiButtonBase-root MuiMenuItem-root MuiMenuItem-gutters Mui-selected MuiMenuItem-root MuiMenuItem-gutters Mui-selected css-1ijhkwz']");
+    //}
+    //private By selectedTime() {
+        //return
+    //}
+    //private By doseFilter() {
+      //  return By.id("button-filter-dosis");
+    //}
+    //private By selectDose() {
+      //  return
+    //}
+    //private By selectedDose() {
+      //  return
+    //}
+    public void openUrl() {
+        openAt("/");
+    }
+    public void goToLoginPage() {
+        $(loginPage()).click();
+    }
+    public void inputEmail(String email) {
+        $(emailField()).type(email);
+    }
+    public void inputPassword(String password) {
+        $(passwordField()).type(password);
+    }
+    public void login() {
+        $(loginButton()).click();
+    }
+    public void goToBookVaksinasiPage() {
+        $(bookVaksinasiPage()).click();
+    }
+    public void selectUnconfirmedSession() {
+        $(unconfirmedSession()).click();
+    }
+    public void approveBookingRequest() {
+        $(checkButton()).click();
+    }
+    public void rejectBookingRequest() {
+        $(xButton()).click();
+    }
+    public void bookingApproved() {
+        $(approved()).isDisplayed();
+    }
+    public void bookingRejected() {
+        $(rejected()).isDisplayed();
+    }
+    public void selectAllBookings() {
+        $(checkboxAllBookings()).click();
+    }
+    public void approveAllBookingRequests() {
+        $(terimaSemuaButton()).click();
+    }
+    public void rejectAllBookingRequests() {
+        $(tolakSemuaButton()).click();
+    }
+    public void sortByDate() {
+        $(calendar()).click();
+    }
+    public void selectAvailableDate() {
+        $(availableDate()).click();
+    }
+    public void availableDateSessionDisplayed() {
+        $(availableDateSession()).isDisplayed();
+    }
+    public void selectUnavailableDate() {
+        $(unavailableDate()).click();
+    }
+    public void noBookingDisplayed() {
+        $(unavailableDateSession()).isDisplayed();
+    }
+    public void sortByVaccine() {
+        $(vaccineFilter()).click();
+    }
+    public void selectVaccine() {
+        $(vaccine()).click();
+    }
+    public void sortedByVaccine() {
+        $(selectedVaccine()).isDisplayed();
+    }
+    public void sortByStatus() {
+        $(statusFilter()).click();
+    }
+    public void selectStatus() {
+        $(status()).click();
+    }
+    public void sortedByStatus() {
+        $(selectedStatus()).isDisplayed();
+    }
+}
 
-}
-}
--- Web Automation/src/test/java/starter/stepdefinitions/ManageVaccinationBookingsSteps.java
+++ Web Automation/src/test/java/starter/stepdefinitions/ManageVaccinationBookingsSteps.java
@@ -4,69 +4,127 @@ import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import net.thucydides.core.annotations.Steps;
+import starter.pages.ManageVaccinationBookingsPage;
 
 public class ManageVaccinationBookingsSteps {
-    @Given("I have logged in")
+    @Steps
+    ManageVaccinationBookingsPage manageVaccinationBookingsPage;
+
+    @Given("I am on landing page")
+    public void iAmOnLandingPage() {
+        manageVaccinationBookingsPage.openUrl();
+    }
+
+    @And("I have logged in")
     public void iHaveLoggedIn() {
+        manageVaccinationBookingsPage.goToLoginPage();
+        manageVaccinationBookingsPage.inputEmail("admininfo@citraarafiqhospital.com");
+        manageVaccinationBookingsPage.inputPassword("rscitraarafiq123");
+        manageVaccinationBookingsPage.login();
     }
 
-    @And("I go to Book Vaksinasi page")
+    @When("I go to Book Vaksinasi page")
     public void iGoToBookVaksinasiPage() {
+        manageVaccinationBookingsPage.goToBookVaksinasiPage();
     }
 
-    @When("I select a session")
+    @And("I select a session")
     public void iSelectASession() {
+        manageVaccinationBookingsPage.selectUnconfirmedSession();
     }
 
     @And("I click check button")
     public void iClickCheckButton() {
+        manageVaccinationBookingsPage.approveBookingRequest();
     }
 
     @Then("the booking will be approved")
     public void theBookingWillBeApproved() {
+        manageVaccinationBookingsPage.bookingApproved();
     }
 
-    @When("I click Terima Semua button")
+    @And("I select all bookings")
+    public void iSelectAllBookings() {
+        manageVaccinationBookingsPage.selectAllBookings();
+    }
+
+    @And("I click Terima Semua button")
     public void iClickTerimaSemuaButton() {
+        manageVaccinationBookingsPage.approveAllBookingRequests();
     }
 
     @And("I click X button")
     public void iClickXButton() {
+        manageVaccinationBookingsPage.rejectBookingRequest();
     }
 
     @Then("the booking will be rejected")
     public void theBookingWillBeRejected() {
+        manageVaccinationBookingsPage.bookingRejected();
     }
 
-    @When("I click Tolak Semua button")
+    @And("I click Tolak Semua button")
     public void iClickTolakSemuaButton() {
+        manageVaccinationBookingsPage.rejectAllBookingRequests();
     }
 
-    @When("I click the arrow button on vaccine field")
+    @And("I click calendar button")
+    public void iClickCalendarButton() {
+        manageVaccinationBookingsPage.sortByDate();
+    }
+
+    @And("I select available date")
+    public void iSelectAvailableDate() {
+        manageVaccinationBookingsPage.selectAvailableDate();
+    }
+
+    @Then("bookings will be sorted by selected date")
+    public void bookingsWillBeSortedBySelectedDate() {
+        manageVaccinationBookingsPage.availableDateSessionDisplayed();
+    }
+
+    @And("I select unavailable date")
+    public void iSelectUnavailableDate() {
+        manageVaccinationBookingsPage.selectUnavailableDate();
+    }
+
+    @Then("no booking will be displayed")
+    public void noBookingWillBeDisplayed() {
+        manageVaccinationBookingsPage.noBookingDisplayed();
+    }
+
+    @And("I click the arrow button on vaccine field")
     public void iClickTheArrowButtonOnVaccineField() {
+        manageVaccinationBookingsPage.sortByVaccine();
     }
 
     @And("I select vaccine")
     public void iSelectVaccine() {
+        manageVaccinationBookingsPage.selectVaccine();
     }
 
     @Then("bookings will be sorted by vaccine")
     public void bookingsWillBeSortedByVaccine() {
+        manageVaccinationBookingsPage.sortedByVaccine();
     }
 
-    @When("I click the arrow button on status field")
+    @And("I click the arrow button on status field")
     public void iClickTheArrowButtonOnStatusField() {
+        manageVaccinationBookingsPage.sortByStatus();
     }
 
     @And("I select status")
     public void iSelectStatus() {
+        manageVaccinationBookingsPage.selectStatus();
     }
 
     @Then("bookings will be sorted by status")
     public void bookingsWillBeSortedByStatus() {
+        manageVaccinationBookingsPage.sortedByStatus();
     }
 
-    @When("I click the dots on the top right of page")
+    @And("I click the dots on the top right of page")
     public void iClickTheDotsOnTheTopRightOfPage() {
     }
 
